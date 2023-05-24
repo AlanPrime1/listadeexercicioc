2a46b5a80a9266bbbe3440496dee9753d12fa453
AlanPrime1 <128154118+AlanPrime1@users.noreply.github.com>
2023-05-24 08:50:53 -0300

    Create questao1.c

---
+++ questao1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float nota;
+
+    printf("Digite uma nota:");
+    scanf("%f", &nota);
+
+    if (nota > 10)
+    {
+        printf("A nota é maior que 10!");
+    }
+    else
+    {
+        printf("Nao e maior que 10!");
+    }
+
+    return 0;
+}
